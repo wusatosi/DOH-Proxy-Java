e7c74be5be39adff60f16075d658c4b8683762da
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-03-15 01:05:23 +0800

    clean-up code

--- src/main/java/wusatosi/LocalServer.java
+++ src/main/java/wusatosi/LocalServer.java
@@ -2,54 +2,74 @@ package wusatosi;
 
 import io.vertx.core.*;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Base64;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class LocalServer {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new Verticle());
+        vertx.deployVerticle(new DOHProxyVerticle());
     }
 
-    static class Verticle extends AbstractVerticle {
+    static class DOHProxyVerticle extends AbstractVerticle {
+        private AtomicInteger count = new AtomicInteger();
+
         @Override
         public void init(Vertx vertx, Context context) {
             super.init(vertx, context);
+            int port = 2020;
             vertx
                     .createHttpServer()
-                    .requestHandler(request -> {
-                        System.out.println(LocalDateTime.now() + " request hit");
-                        HttpServerResponse response = request.response();
-                        request.bodyHandler(buffer -> {
-                            Handler<Promise<Buffer>> makeRequestAsync = (wow) -> {
-                                try {
-                                    byte[] bytes1 = buffer.getBytes();
-                                    System.out.println(Base64.getEncoder().encodeToString(bytes1));
-                                    byte[] bytes = DOHProxy.makeRequest(bytes1);
-                                    wow.complete(Buffer.buffer(bytes));
-                                } catch (IOException e) {
-                                    wow.fail(e);
-                                }
-                            };
-                            Handler<AsyncResult<Buffer>> responseMaker = (wow) -> {
-                                if (wow.failed()) {
-                                    response.setStatusCode(500).end();
-                                    System.out.println(LocalDateTime.now() + " failed, " + wow.cause());
-                                    wow.cause().printStackTrace();
-                                }
-//                                response
-//                                        .setStatusCode(200)
-//                                        .end(wow.result());
-                            };
-                            vertx.executeBlocking(makeRequestAsync, responseMaker);
-                            String s = "AACBgAABAAEAAAABBmdvb2dsZQNjb20AABwAAQZnb29nbGUDY29tAAAcAAEAAAD5ABAkBGgAQAUICQAAAAAAACAOAAApEAAAAAAAAAsACAAHAAEYAHychQ==";
-                            response.setStatusCode(200)
-                                    .end(Buffer.buffer(Base64.getDecoder().decode(s)));
-                        });
-                    }).listen(2020);
+                    .requestHandler(new DOHProxyProcessContext(count.incrementAndGet()))
+                    .listen(port, (server) -> System.out.printf("server started at port %d%n", port));
+        }
+
+        private static class DOHProxyProcessContext implements Handler<HttpServerRequest> {
+            DOHProxyProcessContext(int requestId) {
+                this.requestId = requestId;
+            }
+
+            int requestId;
+
+            @Override
+            public void handle(HttpServerRequest request) {
+                log("request hit");
+                Context context = Vertx.currentContext();
+                request.bodyHandler(body ->
+                        context.executeBlocking(proxyRequest(body), genResponse(request.response())));
+            }
+
+            private void log(String message) {
+                System.out.printf("[%d] %s%n", requestId, message);
+            }
+
+            private Handler<AsyncResult<Buffer>> genResponse(HttpServerResponse response) {
+                return (wow) -> {
+                    if (wow.failed()) {
+                        response.setStatusCode(500).end();
+                        log("proxy request failed, " + wow.cause());
+                        wow.cause().printStackTrace();
+                    }
+                    response
+                            .setStatusCode(200)
+                            .end(wow.result());
+                };
+            }
+
+            private Handler<Promise<Buffer>> proxyRequest(Buffer buffer) {
+                return (wow) -> {
+                    try {
+                        byte[] bytes = DOHProxy.makeRequest(buffer.getBytes());
+                        wow.complete(Buffer.buffer(bytes));
+                    } catch (IOException e) {
+                        wow.fail(e);
+                    }
+                };
+            }
+
         }
     }
 }
