26c8639dc01a1dc87a559b66b64f59dfcafb193d
wusatosi <26424577+wusatosi@users.noreply.github.com>
2020-03-15 01:27:28 +0800

    cleanup AliEventEntry class

--- src/main/java/wusatosi/AliEventEntry.java
+++ src/main/java/wusatosi/AliEventEntry.java
@@ -3,29 +3,59 @@ package wusatosi;
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.HttpRequestHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 
+// invoke by ali-fc service
+@SuppressWarnings("unused")
 public class AliEventEntry implements HttpRequestHandler {
 
     @Override
-    public void handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Context context) throws IOException, ServletException {
-        byte[] request = readALLBytes(httpServletRequest.getInputStream());
+    public void handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Context context) throws IOException {
+        byte[] request;
         try {
+            request = getRequest(httpServletRequest);
             byte[] response = DOHProxy.makeRequest(request);
-            httpServletResponse.setStatus(200);
-            ServletOutputStream outputStream = httpServletResponse.getOutputStream();
-            outputStream.write(response);
-            outputStream.flush();
-            outputStream.close();
+            relayResponse(httpServletResponse, response);
         } catch (IOException e) {
-            e.printStackTrace();
-            httpServletResponse.setStatus(500);
+            relayIOException(e, httpServletResponse);
+        } catch (IllegalArgumentException ignored) {
+            relayArgumentError(httpServletResponse);
         }
+    }
+
+    private void relayResponse(HttpServletResponse httpServletResponse, byte[] response) throws IOException {
+        httpServletResponse.setStatus(200);
+        ServletOutputStream outputStream = httpServletResponse.getOutputStream();
+        outputStream.write(response);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private void relayIOException(IOException e, HttpServletResponse httpServletResponse) throws IOException {
+        System.out.printf("IOException when proxy: %s%n", e);
+        httpServletResponse.sendError(500);
+    }
+
+    private void relayArgumentError(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendError(400);
+    }
+
+    private byte[] getRequest(HttpServletRequest httpServletRequest) {
+        try {
+            String method = httpServletRequest.getMethod().toLowerCase();
+            if (method.equals("post"))
+                return readALLBytes(httpServletRequest.getInputStream());
+            if (method.equals("get"))
+                return Base64.getDecoder().decode(httpServletRequest.getParameter("dns"));
+        } catch (IOException e) {
+            System.out.printf("parse request failed %s%n", e);
+        }
+        throw new IllegalArgumentException();
     }
 
     private static byte[] readALLBytes(InputStream inputStream) throws IOException {
